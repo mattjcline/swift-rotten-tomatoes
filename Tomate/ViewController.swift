b1ed6818514c40b9c648d4e2ab0a1d3bb5927488
Matt Cline <matt.cline@rd.io>
2015-04-14 17:47:32 -0700

    Initial Commit

---
+++ Tomate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tomate
+//
+//  Created by Matt Cline on 4/14/15.
+//  Copyright (c) 2015 Matt Cline. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
