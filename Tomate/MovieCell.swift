58c42397eb60657acd5d2749d180f60872eac1b6
Matt Cline <matt.cline@rd.io>
2015-04-20 22:26:35 -0700

    All required user stories working

---
+++ Tomate/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Tomate
+//
+//  Created by Matt Cline on 4/17/15.
+//  Copyright (c) 2015 Matt Cline. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        titleLabel.text.adjustsFontSizeToFitWidth = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Tomate/MovieDetailsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MovieDetailsViewController.swift
+//  Tomate
+//
+//  Created by Matt Cline on 4/17/15.
+//  Copyright (c) 2015 Matt Cline. All rights reserved.
+//
+
+import UIKit
+
+class MovieDetailsViewController: UIViewController {
+
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    
+    var movie: NSDictionary!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        titleLabel.text = movie["title"] as? String
+        synopsisLabel.text = movie["synopsis"] as? String
+        
+        let loadingNotification = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        loadingNotification.mode = MBProgressHUDMode.Indeterminate
+        loadingNotification.labelText = "🎬"
+        
+        var url = movie.valueForKeyPath("posters.original") as! String!
+        var range = url.rangeOfString(".*cloudfront.net/", options: .RegularExpressionSearch)
+        if let range = range {
+            url = url.stringByReplacingCharactersInRange(range, withString: "https://content6.flixster.com/")
+        }
+        
+        let nsUrl = NSURL(string: url)
+        posterView.setImageWithURL(nsUrl)
+        MBProgressHUD.hideAllHUDsForView(self.view, animated: true)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
