6b901d0ce1215cbb3218797d82dee87b66a1da71
Matt Cline <matt.cline@rd.io>
2015-04-20 22:58:49 -0700

    v1.0

---
+++ Pods/Target Support Files/Pods-Tomate-AFNetworking/Pods-Tomate-AFNetworking-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tomate_AFNetworking : NSObject
+@end
+@implementation PodsDummy_Pods_Tomate_AFNetworking
+@end
---
+++ Pods/Target Support Files/Pods-Tomate/Pods-Tomate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Tomate : NSObject
+@end
+@implementation PodsDummy_Pods_Tomate
+@end
